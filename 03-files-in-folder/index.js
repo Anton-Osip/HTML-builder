30ac09f2f9345401c7d2d5d602090141d60138d6
Anton-Osip <osipchika@gmail.com>
2024-01-16 20:14:20 +0300

    feat: implement files in folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+const {stdin, stdout} = require("process")
+
+fs.promises.readdir(path.join(__dirname,'secret-folder'),{withFileTypes:true}).then(res=>{
+    res.forEach(result=>{
+        if(!result.isDirectory()){
+            const filePath = path.join(__dirname,'secret-folder',result.name)
+            const fileName = path.parse(filePath).name
+            const exit = path.parse(filePath).ext
+            fs.promises.stat(filePath).then(res=>{
+                console.log(`${fileName} - ${exit.replace('.','')} - ${res.size}`);
+            })
+        }
+    })
+})
